d14d9f8b626dd6f215130d722abc9140479aa070
l3miage-couliban <n-djo-soro.coulibaly@etu.univ-grenoble-alpes.fr>
2024-12-26 21:22:32 +0100

    Initial commit

---
+++ src/main/java/fr/uga/miage/m1/projetIntegrateur/ProjetIntegrateurApplication.java
@@ -0,0 +1,13 @@
+package fr.uga.miage.m1.projetIntegrateur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntegrateurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetIntegrateurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/uga/miage/m1/projetIntegrateur/ProjetIntegrateurApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.uga.miage.m1.projetIntegrateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegrateurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
